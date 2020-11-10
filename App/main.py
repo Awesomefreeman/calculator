f6b91287f0adaaf6a008a160729a96aad29c8e5c
Awesomefreeman <>
2020-11-10 16:10:19 +0300

    Добавлены действия: вычитание, умножение, деление

--- App/main.py
+++ App/main.py
@@ -1,28 +1,52 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Calculator")
-root.geometry("300x200")
-text_box1 = tk.Entry(root, width=20)
-text_box2 = tk.Entry(root, width=20)
-button_multiple = tk.Button(root, text="+")
-label = tk.Label(root, bg='black', fg='white')
-text_box1.pack()
-text_box2.pack()
-button_multiple.pack()
-label.pack()
 
+class Actions:
+    def __init__(self, root):
+        self.text_box1 = tk.Entry(root, width=20)
+        self.text_box2 = tk.Entry(root, width=20)
+        self.button_addition = tk.Button(root, text="+")
+        self.button_subtraction = tk.Button(root, text="-")
+        self.button_multiplication = tk.Button(root, text="*")
+        self.button_division = tk.Button(root, text="/")
+        self.button_addition["command"] = self.addition
+        self.button_subtraction["command"] = self.subtraction
+        self.button_multiplication["command"] = self.multiplication
+        self.button_division["command"] = self.division
+        self.label = tk.Label(root, width=20, bg='black', fg='white', text="Ответ")
+        for button in self.text_box1, self.text_box2, \
+                      self.button_addition, self.button_subtraction, \
+                      self.button_multiplication, self.button_division, self.label:
+            button.pack()
 
-def multiple(event):
-    text1 = text_box1.get()
-    text2 = text_box2.get()
-    label["text"] = int(text1) + int(text2)
+    def addition(self):
+        first_number = self.text_box1.get()
+        second_number = self.text_box2.get()
+        self.label["text"] = int(first_number) + int(second_number)
+
+    def subtraction(self):
+        first_number = self.text_box1.get()
+        second_number = self.text_box2.get()
+        self.label["text"] = int(first_number) - int(second_number)
+
+    def multiplication(self):
+        first_number = self.text_box1.get()
+        second_number = self.text_box2.get()
+        self.label["text"] = int(first_number) * int(second_number)
+
+    def division(self):
+        first_number = self.text_box1.get()
+        second_number = self.text_box2.get()
+        self.label["text"] = int(first_number) / int(second_number)
 
 
 def main():
-    button_multiple.bind("<Button-1>", multiple)
+    root = tk.Tk()
+    root.title("Calculator")
+    root.geometry("300x200")
+    action = Actions(root)
     root.mainloop()
-    print(__name__)
+
 
 if __name__ == '__main__':
     main()
