31b35a8b94dddc3f00ac9c81aa62eb5ecb9127a3
Awesomefreeman <>
2020-11-11 01:21:07 +0300

    Добавлен плейсхолдер. Увеличен размер элементов

--- App/main.py
+++ App/main.py
@@ -4,17 +4,27 @@ import re
 
 class Buttons:
     def __init__(self, root):
-        self.text_box1 = tk.Entry(root, width=20)
-        self.text_box2 = tk.Entry(root, width=20)
-        self.button_addition = tk.Button(root, text="+")
-        self.button_subtraction = tk.Button(root, text="-")
-        self.button_multiplication = tk.Button(root, text="*")
-        self.button_division = tk.Button(root, text="/")
-        self.label = tk.Label(root, width=20, bg='black', fg='white', text="Ответ")
+        self.text_box1 = tk.Entry(root, width=20, font=("System", 20))
+        self.text_box1.insert(0, "Введите число 1")
+        self.text_box2 = tk.Entry(root, width=20, font=("System", 20))
+        self.text_box2.insert(0, "Введите число 2")
+        self.button_addition = tk.Button(root, text="+", font=("System", 20))
+        self.button_subtraction = tk.Button(root, text="-", font=("System", 20))
+        self.button_multiplication = tk.Button(root, text="*", font=("System", 20))
+        self.button_division = tk.Button(root, text="/", font=("System", 20))
+        self.label = tk.Label(root, width=20, fg='black', text="Ответ", relief="ridge", font=("System", 20)) # flat, groove, raised, ridge, solid, or sunken
         for button in self.text_box1, self.text_box2, \
                       self.button_addition, self.button_subtraction, \
                       self.button_multiplication, self.button_division, self.label:
             button.pack()
+        self.text_box1.bind("<Button-1>", self.on_click_text_box1)
+        self.text_box2.bind("<Button-1>", self.on_click_text_box2)
+
+    def on_click_text_box1(self, event):
+        self.text_box1.delete(0, tk.END)
+
+    def on_click_text_box2(self, event):
+        self.text_box2.delete(0, tk.END)
 
 
 class Actions(Buttons):
@@ -65,7 +75,7 @@ class Actions(Buttons):
 def main():
     root = tk.Tk()
     root.title("Calculator")
-    root.geometry("300x200")
+    root.geometry("300x250")
     action = Actions(root)
     root.mainloop()
 
